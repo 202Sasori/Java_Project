35a8b539d49a4fcf6f0ffb30e86bb43bff05b07f
202Sasori <65811291+202Sasori@users.noreply.github.com>
2021-10-10 15:49:43 +0700

    BTL_Java

---
+++ BTL-QLTV/src/DATABASE/Account.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Account
+USE Account
+
+CREATE TABLE Account(
+	UserName VARCHAR(50),
+	Pass CHAR(20)
+)
+INSERT INTO Account(UserName,Pass)
+VALUES ('dinhkhachoat','20200184'),
+        ('tranductien','20200781'),
+	('daovietbao','20200910')
